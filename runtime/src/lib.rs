05ea0ab1046b9473382f4e93a5daa044673d8f9a
Donny <kdy1997.dev@gmail.com>
2021-11-03 15:01:52 +0900

    Some work

--- runtime/src/lib.rs
+++ runtime/src/lib.rs
@@ -1,13 +1,2 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
-    }
-}
-
-#[no_mangle]
-pub extern "C" fn foo() -> usize {
-    5
-}
+/// Force linking
+extern crate kdy1_test_common;
